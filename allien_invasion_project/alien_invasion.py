357f02732270dd8978ceb49501eee441919148e6
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-09-11 00:11:58 +0330

    initial commit

---
+++ allien_invasion_project/alien_invasion.py
@@ -0,0 +1,9 @@
+import pygame
+
+
+class AlienInvasion:
+    """Overall class to manage game bahvior"""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
