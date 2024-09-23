1c9372bb62dd99e92ff15cf6ae3d4eaf68d249a7
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-09-23 06:07:32 +0330

    Create the fleet of aliens

--- allien_invasion_project/alien_invasion.py
+++ allien_invasion_project/alien_invasion.py
@@ -75,9 +75,20 @@ class AlienInvasion:
     
     def create_fleet(self):
         """Create the fleet of aliens."""    
-        # Make an alien
+        #Create an alien and find the number of aliens in a row.
+        #Spacing between each alien is equal to one alien width.
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Create the first row of alians.
+        for alien_number in range(number_aliens_x):
+            # Create an alien and place it in the row
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     def update_screen(self):
         """Update images on the screen, and flip to the new screen."""
