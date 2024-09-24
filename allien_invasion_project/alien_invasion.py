27420bac175aa6200afb68e0f3d944d9b57abc01
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-09-24 17:29:26 +0330

    add groupcollide in update_bullets method

--- allien_invasion_project/alien_invasion.py
+++ allien_invasion_project/alien_invasion.py
@@ -73,6 +73,12 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+                
+        # Check for any bullets that have hit aliens
+        # If so, get rid of the bullet and the alien
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True
+        ) 
     
     def update_aliens(self):
         """
