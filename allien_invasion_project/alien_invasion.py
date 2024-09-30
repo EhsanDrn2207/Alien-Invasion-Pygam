df0dc90ed53d3eb111db65a445cb957fb6b7ef97
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-09-30 09:44:15 +0330

    add check_aliens_bottom method in AI

--- allien_invasion_project/alien_invasion.py
+++ allien_invasion_project/alien_invasion.py
@@ -111,6 +111,15 @@ class AlienInvasion:
 
         # Pause
         sleep(0.5)
+        
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.bottom >= screen_rect.bottom:
+                # Treat this the same as if the shp got hit.
+                self._ship_hit()
+                break
 
     def _update_aliens(self):
         """
@@ -123,6 +132,9 @@ class AlienInvasion:
         # Look for alien-ship collisions
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
+            
+        # Look for aliens hitting the bottom of the screen
+        self._check_aliens_bottom()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
